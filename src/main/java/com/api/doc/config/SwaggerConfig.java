36429f96b6b1b6d18b3676799b1920a15d763b76
sooyun <55366664+imcodding@users.noreply.github.com>
2022-05-26 14:57:16 +0900

    Swagger 적용
    Swagger 3.0.0

---
+++ src/main/java/com/api/doc/config/SwaggerConfig.java
@@ -0,0 +1,38 @@
+package com.api.doc.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public Docket restAPI() {
+        return new Docket(DocumentationType.OAS_30)
+                .useDefaultResponseMessages(false)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.api.doc.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Practice Swagger")
+                .description("API 문서 설명입니다.")
+                .version("1.0")
+                .build();
+    }
+}
---
+++ src/main/java/com/api/doc/controller/SwaggerController.java
@@ -0,0 +1,34 @@
+package com.api.doc.controller;
+
+import com.api.doc.dto.Board;
+import com.api.doc.repository.BoardRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class SwaggerController {
+
+    private final BoardRepository boardRepository;
+
+    @Operation(summary = "게시글 목록 조회 API", description = "Bearer 토큰이 필요합니다.")
+    @GetMapping("/api/v1/board/list")
+    public List<Board> getBoardList() {
+        return boardRepository.findAll();
+    }
+
+    @Operation(summary = "게시글 상세 조회 API", description = "Bearer 토큰이 필요합니다.")
+    @GetMapping("/api/v1/board/{boardId}")
+    public Board getBoard(@PathVariable Long boardId) {
+        return boardRepository.findById(boardId);
+    }
+
+    @Operation(summary = "게시글 등록 API", description = "Bearer 토큰이 필요합니다.")
+    @PostMapping("/api/v1/board")
+    public Board addBoard(@RequestBody Board board) {
+        return boardRepository.save(board);
+    }
+}
